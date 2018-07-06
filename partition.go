c8ff13ef4bc9344a997693f42f665e2304a21086
Leo Adamek <leo.adamek@mrzen.com>
2018-07-06 14:49:00 +0100

    Add MacPartitioner()

--- partition.go
+++ partition.go
@@ -2,6 +2,7 @@ package ksuid
 
 import (
 	"encoding/binary"
+	"net"
 )
 
 // PartitionFunc is a func which returns a partition for the
@@ -19,4 +20,27 @@ func StringPartitioner(v string) PartitionFunc {
 // NilPartitioner places everything into partition zero, disabling partitioning
 func NilPartitioner() uint32 {
 	return 0
+}
+
+// MacPartitioner places everything into a partition based on the last 4 bytes of the primary network mac address
+// It's a wrapped in a func which may error if
+func MacPartitioner() (PartitionFunc, error) {
+
+	interfaces, err := net.Interfaces()
+
+	if err != nil { return nil, err }
+
+	for _, i := range interfaces {
+		if len(i.HardwareAddr) > 0 {
+			a := i.HardwareAddr[2:]
+
+			v := binary.BigEndian.Uint32(a)
+
+			return func() uint32 {
+				return v
+			}, nil
+		}
+	}
+
+	return nil, nil
 }
--- partition_test.go
+++ partition_test.go
@@ -2,7 +2,7 @@ package ksuid
 
 import (
 	"testing"
-	)
+)
 
 func TestStringPartitioner(t *testing. T) {
 	p1 := StringPartitioner("test")
@@ -19,8 +19,41 @@ func TestStringPartitioner(t *testing. T) {
 	}
 }
 
+func BenchmarkStringPartitioner(b *testing.B) {
+	p := StringPartitioner("test")
+
+	for i := 0; i < b.N; i++ {
+		p()
+	}
+}
+
 func TestNilPartitioner(t *testing.T) {
 	if v := NilPartitioner(); v != 0 {
 		t.Fail()
+	}
+}
+
+func TestMacPartitioner(t *testing.T) {
+	fn, err := MacPartitioner()
+
+	if err != nil || fn == nil {
+		t.Error(err)
+		t.FailNow()
+	}
+
+	if fn() == 0 {
+		t.Error("MacPartitioner()() must be >0")
+		t.Fail()
+	}
+}
+func BenchmarkMacPartitioner(b *testing.B) {
+	p, err := MacPartitioner()
+
+	if err != nil {
+		b.FailNow()
+	}
+
+	for i := 0; i < b.N; i++ {
+		p()
 	}
 }
